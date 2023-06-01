164abe59980c304dc62d2b05abc5857f49f3572d
Vijaylaxmi Pujari <vijuaxmipujari23@gmail.com>
2023-06-01 19:53:38 +0530

    first Commit

---
+++ Research_notebooks/deep_learning_perceptron.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import joblib
+from matplotlib.colors import ListedColormap
+
+plt.style.use("fivethirtyeight") ## << optional step
+
+
+# In[3]:
+
+
+class Perceptron:
+    def __init__(self, eta: float=None, epochs: int=None):
+        self.weights = np.random.randn(3) * 1e-4 # small random weights
+        training = (eta is not None) and (epochs is not None)
+        if training:
+            print(f"initial weights before training: \n{self.weights}\n")
+        self.eta = eta
+        self.epochs = epochs
+    
+    def _z_outcome(self, inputs, weights):
+        return np.dot(inputs, weights)
+    
+    def activation_function(self, z):
+        return np.where(z > 0, 1, 0)
+    
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        
+        X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))]
+        print(f"X with bias: \n{X_with_bias}")
+        
+        for epoch in range(self.epochs):
+            print("--"*10)
+            print(f"for epoch >> {epoch}")
+            print("--"*10)
+            
+            z = self._z_outcome(X_with_bias, self.weights)
+            y_hat = self.activation_function(z)
+            print(f"predicted value after forward pass: \n{y_hat}")
+            
+            self.error = self.y - y_hat
+            print(f"error: \n{self.error}")
+            
+            self.weights = self.weights + self.eta * np.dot(X_with_bias.T, self.error)
+            print(f"updated weights after epoch: {epoch + 1}/{self.epochs}: \n{self.weights}")
+            print("##"*10)
+            
+    
+    def predict(self, X):
+        X_with_bias = np.c_[X, -np.ones((len(X), 1))]
+        z = self._z_outcome(X_with_bias, self.weights)
+        return self.activation_function(z)
+    
+    def total_loss(self):
+        total_loss = np.sum(self.error)
+        print(f"\ntotal loss: {total_loss}\n")
+        return total_loss
+    
+    def _create_dir_return_path(self, model_dir, filename):
+        os.makedirs(model_dir, exist_ok=True)
+        return os.path.join(model_dir, filename)
+    
+    def save(self, filename, model_dir=None):
+        if model_dir is not None:
+            model_file_path = self._create_dir_return_path(model_dir, filename)
+            joblib.dump(self, model_file_path)
+        else:
+            model_file_path = self._create_dir_return_path("model", filename)
+            joblib.dump(self, model_file_path)
+            
+    def load(self, filepath):
+        return joblib.load(filepath)
+
+
+# In[4]:
+
+
+def prepare_data(df, target_col="y"):
+    X = df.drop(target_col, axis=1)
+    
+    y = df[target_col]
+    
+    return X, y 
+
+
+# In[5]:
+
+
+AND = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y" : [0,0,0,1]
+}
+
+df_AND = pd.DataFrame(AND)
+
+df_AND
+
+
+# In[6]:
+
+
+X, y = prepare_data(df_AND)
+
+ETA = 0.1 # 0 and 1
+EPOCHS = 10
+
+model_and = Perceptron(eta=ETA, epochs=EPOCHS)
+model_and.fit(X, y)
+
+_ = model_and.total_loss()
+
+
+# In[7]:
+
+
+model_and.save(filename="and.model")
+
+
+# In[8]:
+
+
+reload_model_and = Perceptron().load(filepath="model/and.model")
+
+
+# In[9]:
+
+
+reload_model_and.predict(X=[[1,1]])
+
+
+# In[10]:
+
+
+OR = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y" : [0,1,1,1]
+}
+
+df_OR = pd.DataFrame(OR)
+
+df_OR
+
+
+# In[11]:
+
+
+X, y = prepare_data(df_OR)
+
+ETA = 0.1 # 0 and 1
+EPOCHS = 10
+
+model_or = Perceptron(eta=ETA, epochs=EPOCHS)
+model_or.fit(X, y)
+
+_ = model_or.total_loss()
+
+
+# In[12]:
+
+
+model_or.save(filename="or.model", model_dir="model_or")
+
+
+# In[13]:
+
+
+XOR = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y" : [0,1,1,0]
+}
+
+df_XOR = pd.DataFrame(XOR)
+
+df_XOR
+
+
+# In[14]:
+
+
+X, y = prepare_data(df_XOR)
+
+ETA = 0.1 # 0 and 1
+EPOCHS = 10
+
+model_xor = Perceptron(eta=ETA, epochs=EPOCHS)
+model_xor.fit(X, y)
+
+_ = model_xor.total_loss()
+
+
+# In[15]:
+
+
+def save_plot(df, model, filename="plot.png", plot_dir="plots"):
+    def _create_base_plot(df):
+        df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="coolwarm")
+        plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
+        plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
+        
+        figure = plt.gcf()
+        figure.set_size_inches(10, 8)
+    
+    def _plot_decision_regions(X, y, classifier, resolution=0.02):
+        colors = ("cyan", "lightgreen")
+        cmap = ListedColormap(colors)
+        
+        X = X.values # as an array
+        x1 = X[:, 0]
+        x2 = X[:, 1]
+        
+        x1_min, x1_max = x1.min() - 1, x1.max() + 1 
+        x2_min, x2_max = x2.min() - 1, x2.max() + 1
+        
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                               np.arange(x2_min, x2_max, resolution)
+                              )
+        y_hat = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        y_hat = y_hat.reshape(xx1.shape)
+        
+        plt.contourf(xx1, xx2, y_hat, alpha=0.3, cmap=cmap)
+        plt.xlim(xx1.min(), xx1.max())
+        plt.ylim(xx2.min(), xx2.max())
+        plt.plot()
+        
+    X, y = prepare_data(df)
+    
+    _create_base_plot(df)
+    _plot_decision_regions(X, y, model)
+    
+    os.makedirs(plot_dir, exist_ok=True)
+    plot_path = os.path.join(plot_dir, filename)
+    plt.savefig(plot_path)
+
+
+# In[16]:
+
+
+save_plot(df_OR, model_or, filename="or.png")
+
+
+# In[17]:
+
+
+save_plot(df_AND, model_and, filename="and.png")
+
+
+# In[18]:
+
+
+save_plot(df_XOR, model_xor, filename="xor.png")
+
+
+# In[ ]:
+
+
+
+
